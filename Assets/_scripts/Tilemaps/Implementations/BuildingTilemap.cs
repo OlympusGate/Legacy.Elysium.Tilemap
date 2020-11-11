83b33a50efad7cbf12b2df23efffa6dc49ddc4df
jonathan_aykroyd <jonathan.aykroyd@wildlifestudios.com>
2020-11-11 05:08:46 +0000

    removed unused packages

--- Assets/_scripts/Tilemaps/Implementations/BuildingTilemap.cs
+++ Assets/_scripts/Tilemaps/Implementations/BuildingTilemap.cs
@@ -1,52 +1,9 @@
-﻿using Elysium.Timers;
-using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Elysium.Tilemaps
 {
-    public class BuildingTileObject : SmartTileObject
-    {
-        public TestBuildingObject building { get; private set; }
-        public GameObject gameObject { get; set; }
-        public bool functional { get; set; }
-        public TimerInstance activeStatusTimer { get; set; }
-
-        public BuildingTileObject(Vector3Int _position, Action<Vector3Int> _callback, TestBuildingObject _building) : base(_position, _callback)
-        {
-            this.building = _building;
-            this.functional = true;
-
-            this.activeStatusTimer = Timer.CreateTimer(10f, false);
-            this.activeStatusTimer.OnTimerEnd += Deactivate;
-        }
-
-        ~BuildingTileObject()
-        {
-            activeStatusTimer.isDestroyed = true;
-            activeStatusTimer.SetTime(0.1f);
-        }
-
-        public void Deactivate()
-        {
-            if (!functional) { return; }
-
-            functional = false;
-            activeStatusTimer.SetTime(0f);
-            TriggerOnValueChanged(position);
-        }
-
-        public void Repair()
-        {
-            if (functional) { return; }
-
-            functional = true;
-            activeStatusTimer.SetTime(10f);
-            TriggerOnValueChanged(position);
-        }
-    }
-
     public class BuildingTilemap : SmartTilemap<BuildingTileObject>
     {
         public BuildingTileObject GetBuilding(Vector2 _position)
